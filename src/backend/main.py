ea5133e64388424a4beb9511d7dfb526b689085a
Natalia Pavlovskaia <ya-ne-bo@yandex.ru>
2026-08-18 21:07:07 +0200

    avoid file duplicates uploads

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,80 +1,85 @@
 import os
+import io
+import json
 from pathlib import Path
 from typing import Any, Dict, List
-from langchain.chat_models import init_chat_model
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_core.prompts import PromptTemplate
 from langchain_classic.chains import create_retrieval_chain
-#from langchain_core.retrievers import create_retrieval_chain
 from langchain_classic.chains.combine_documents import (
     create_stuff_documents_chain,
 )
 from langchain_core.prompts import ChatPromptTemplate
-from load_data import load_all_from_folder
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
-from fastapi import FastAPI, HTTPException, UploadFile, File, Response
+from fastapi import FastAPI, HTTPException, UploadFile, File
 from fastapi.responses import StreamingResponse
 from dotenv import load_dotenv
+from pathlib import Path
+from langchain_core.documents import Document
+from pypdf import PdfReader
+from docx import Document as DocxDocument
+
+import hashlib
+
+HASHES_FILE = "backend/processed_hashes.json"
 
 load_dotenv()
 
 app = FastAPI(title="Simple React Backend")
 
 
+def compute_content_hash(content):
+    return hashlib.sha256(content).hexdigest()
 
+
+
+def load_processed_hashes():
+    if os.path.exists(HASHES_FILE):
+        with open(HASHES_FILE, "r") as f:
+            return set(json.load(f))
+    return set()
+
+def save_processed_hashes(hashes):
+    os.makedirs(os.path.dirname(HASHES_FILE), exist_ok=True)
+    with open(HASHES_FILE, "w") as f:
+        json.dump(list(hashes), f)
 
 @app.post("/api/research")
-async def do_rag(data: Dict[str, str]) -> Dict[str, str]:
-    # Путь к сохранённому индексу
+async def do_rag(data: Dict[str, str]):
     faiss_path = "backend/faiss_index"
 
-    # ВАЖНО: тот же тип эмбеддингов, что и при создании индекса
     embeddings = OpenAIEmbeddings()
 
-    # Загрузка FAISS из локальной папки
     db = FAISS.load_local(folder_path=faiss_path, embeddings=embeddings,
                           allow_dangerous_deserialization=True)
 
-    retriever = db.as_retriever(search_kwargs={"k": 3})  # k = сколько результатов вернуть
+    retriever = db.as_retriever(search_kwargs={"k": 3})
 
     template = """You are the best documentation reader and explanator.
     Use the context to answer the research query from the user. 
     Use markdown in your answer. For example bullet points or titles.
     If you do not have enough information, answer that you need more documentation. 
     
-    Context:
-    {context}
+    Context: {context}
     
     Research query: {input}
     """
     prompt = ChatPromptTemplate.from_template(template=template)
-    # llm = init_chat_model("gpt-3.5-turbo-0125")
-    #
-    # question_answer_chain = create_stuff_documents_chain(llm, prompt)
-    # print('data ', data)
-    # qa_chain = create_retrieval_chain(retriever, question_answer_chain)
-
     llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
     combine_docs_chain = create_stuff_documents_chain(llm, prompt)
 
-    #qa_chain = create_retrieval_chain(retriever, prompt | llm)
     qa_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
 
     async def generate(query):
         async for chunk in qa_chain.astream({"input": query['request']}):
-            print('chunk!', chunk)
             text = chunk.get("answer", "")
-            yield text #f"data: {text}\n\n"
+            yield text
 
-
-    #return {"message": "Everything is fine!"}
     to_return = StreamingResponse(
         content=generate(data),
         media_type="text/markdown",
-        #media_type="text/event-stream",
         headers={
             "Cache-Control": "no-cache",
             "X-Accel-Buffering": "no",
@@ -85,52 +90,62 @@ async def do_rag(data: Dict[str, str]) -> Dict[str, str]:
 
 
 @app.post("/api/sources")
-async def upload_files(files: List[UploadFile] = File(...)) -> Dict[str, List[Dict[str, Any]]]:
-    upload_dir = Path(os.getenv("UPLOAD_DIR", "./uploads"))
-    upload_dir.mkdir(parents=True, exist_ok=True)
-
-    saved_paths = []
+async def upload_files(files: List[UploadFile] = File(...)):
+    processed_hashes = load_processed_hashes()
     to_return = []
     docs = []
     for f in files:
         safe_name = f.filename.replace("\\", "/").split("/")[-1]
         if not safe_name:
             raise HTTPException(status_code=400, detail="Empty filename")
+        extension = f.filename.lower().split('.')[-1]
+        content_bytes = await f.read()
+        file_hash = compute_content_hash(content_bytes)
 
-        file_path = upload_dir / safe_name
-        with open(file_path, "wb") as out_file:
-            out_file.write(await f.read())
+        if file_hash not in processed_hashes:
+            processed_hashes.add(file_hash)
+            ff = ""
+            if extension not in ["pdf", "doc", "docx"]:
+                ff = content_bytes.decode("utf-8",  errors="ignore")
+            elif extension == "pdf":
+                reader = PdfReader(io.BytesIO(content_bytes))
+                ff = ""
+                for page in reader.pages:
+                    ff += page.extract_text() or ""
+            elif extension in ['doc', 'docx']:
+                doc = DocxDocument(io.BytesIO(content_bytes))
+                ff = "\n".join([para.text for para in doc.paragraphs])
+            doc = Document(
+                page_content=ff,
+                metadata={"source": safe_name}
+            )
+            docs.append(doc)
 
-        saved_paths.append({"filename": safe_name, "path": str(file_path.absolute())})
         to_return.append({
             "name": safe_name,
             "size": f.size,
             "type": f.content_type
         })
-
-    docs = load_all_from_folder("uploads")
+    save_processed_hashes(processed_hashes)
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=800,
         chunk_overlap=100,
     )
-    print('docs ', docs)
-    splitted_docs = text_splitter.split_documents(docs)
-    print('splitted docs ', splitted_docs)
+    if len(docs) > 0:
+        split_docs = text_splitter.split_documents(docs)
+        embeddings = OpenAIEmbeddings()
 
-    embeddings = OpenAIEmbeddings()
-    print('embeddings ', embeddings)
+        faiss_index_path = "backend/faiss_index"
+        if os.path.exists(faiss_index_path):
+            db = FAISS.load_local(
+                folder_path=faiss_index_path,
+                embeddings=embeddings,
+                allow_dangerous_deserialization=True
+            )
+            db.add_documents(split_docs)
+        else:
+            db = FAISS.from_documents(split_docs, embeddings)
 
-    faiss_index_path = "backend/faiss_index"
-    os.makedirs(faiss_index_path, exist_ok=True)
-
-
-    db = FAISS.from_documents(
-        documents=splitted_docs,
-        embedding=embeddings
-    )
-
-    db.save_local(faiss_index_path)
-
-
+        db.save_local(faiss_index_path)
 
     return {"uploaded": to_return}
